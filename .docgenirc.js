91b001aa06f2e79d21c630e43103932d531eaeed
care <710783534@qq.com>
2021-12-02 20:32:10 +0800

    a

--- .docgenirc.js
+++ .docgenirc.js
@@ -2,9 +2,10 @@
  * @type {import('@docgeni/template').DocgeniConfig}
  */
 module.exports = {
-  mode: "full",
-  title: "care",
-  logoUrl: "https://care526.github.io/logo.jpeg",
-  docsDir: "docs",
-  repoUrl: "https://github.com/care526",
+  mode: 'full',
+  title: 'care',
+  logoUrl: 'https://care526.github.io/logo.jpeg',
+  docsDir: 'docs',
+  outputDir: 'dist',
+  repoUrl: 'https://github.com/care526',
 };
